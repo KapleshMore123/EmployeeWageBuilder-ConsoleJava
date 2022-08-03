86151ed8f307cd18662744ba068ba491e0257302
KalpeshMore123 <morekalpesh1998@gmail.com>
2022-08-03 09:56:56 +0530

    MODIFIED:[Kalpesh] Check Employee Attendance

--- src/com/infogalaxy/EmployeeWage.java
+++ src/com/infogalaxy/EmployeeWage.java
@@ -5,6 +5,15 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		// UC-1 Check Employee Attendance
+		int checkAttendance = (int)(Math.random()*10)%2;
+		System.out.println(checkAttendance);
+		if(checkAttendance == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
